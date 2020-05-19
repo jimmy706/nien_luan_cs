db5ec5f74cdea4602f0515a5af64a8db05b8a549
jimmy <jim.dangquoc706@gmail.com>
2020-05-19 21:48:18 +0700

    add colors for labels

---
+++ src/components/CardDetailModal/CardDetailModal.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CardDetailModal extends Component {
+    render() {
+        return (
+            <div className="card-detail">
+                
+            </div>
+        );
+    }
+}
+
+export default CardDetailModal;
---
+++ src/constants/label-colors.js
@@ -0,0 +1,15 @@
+const colors = [
+    "#61bd50",
+    "#f1d600",
+    "#ff9f1a",
+    "#eb5a47",
+    "#c378e1",
+    "#0079be",
+    "#00c1e0",
+    "#51e897",
+    "#ff78cc",
+    "#344563",
+    "#b3b9c5"
+];
+
+export default colors;
