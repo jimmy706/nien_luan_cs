359b7f60dcf780a7ef1c6d09cc800eec59d19a55
jimmy <jim.dangquoc706@gmail.com>
2020-06-13 11:07:25 +0700

    change base URL

--- src/constants/_global-vars.js
+++ src/constants/_global-vars.js
@@ -2,4 +2,4 @@ let isDev = false;
 
 export const BASE_URL = isDev
   ? "http://localhost:3001/api"
-  : "http://ec2-52-4-10-165.compute-1.amazonaws.com:3001/api";
+  : "https://nien-luan-cs-sv.herokuapp.com/";
