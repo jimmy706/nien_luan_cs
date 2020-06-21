15d51ec61cca37f74a836b9c5db7ee9c71e4a12d
jimmy <jim.dangquoc706@gmail.com>
2020-06-21 10:44:40 +0700

    add disabled delete board for Member role

--- src/components/BoardCard/BoardCard.js
+++ src/components/BoardCard/BoardCard.js
@@ -1,34 +1,55 @@
-import React from 'react';
+import React from "react";
 import Link from "next/link";
-import {CommandBar} from "office-ui-fabric-react";
-import {deleteBoardAction} from "../../store/actions/boards.action";
-import {connect} from "react-redux";
-import {useRouter} from "next/router";
+import { CommandBar } from "office-ui-fabric-react";
+import { deleteBoardAction } from "../../store/actions/boards.action";
+import { connect } from "react-redux";
+import { useRouter } from "next/router";
+import { checkCurrentIsAdmin } from "../../helpers/auth";
+
 function BoardCard(props) {
-    const router = useRouter();
-    const {theme,boardName, _id} = props.board;
-    const overflowItem = [
-        { key: 'view', text: 'View detail', onClick: () => {router.push("/b/[boardId]", "/b/" + _id)}, iconProps: { iconName: 'RedEye' } },
-        { key: 'delete', text: 'Delete board', onClick: () => props.deleteBoardAction(_id), iconProps: { iconName: 'RemoveFromTrash' } },
-    ];
-    return (
-        <div className="board-card" style={{
-            background: theme
-        }}>
-            <div className={"dropdown-container"}>
-                <CommandBar
-                    overflowItems={overflowItem}
-                />
-            </div>
-            <Link href={"/b/[boardId]"} as={"/b/" + _id}>
-                <a className="board-name">
-                    <h3>
-                        {boardName}
-                    </h3>
-                </a>
-            </Link>
-        </div>
-    )
+  const router = useRouter();
+  const { theme, boardName, _id } = props.board;
+
+  const overflowItem = [
+    {
+      key: "view",
+      text: "View detail",
+      onClick: () => {
+        router.push("/b/[boardId]", "/b/" + _id);
+      },
+      iconProps: { iconName: "RedEye" },
+    },
+    {
+      key: "delete",
+      text: "Delete board",
+      onClick: () => props.deleteBoardAction(_id),
+      iconProps: { iconName: "RemoveFromTrash" },
+      disabled: !checkCurrentIsAdmin(props.board, props.user),
+    },
+  ];
+  return (
+    <div
+      className="board-card"
+      style={{
+        background: theme,
+      }}
+    >
+      <div className={"dropdown-container"}>
+        <CommandBar overflowItems={overflowItem} />
+      </div>
+      <Link href={"/b/[boardId]"} as={"/b/" + _id}>
+        <a className="board-name">
+          <h3>{boardName}</h3>
+        </a>
+      </Link>
+    </div>
+  );
 }
 
-export default connect(null,{deleteBoardAction})(BoardCard);
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+export default connect(mapStateToProps, { deleteBoardAction })(BoardCard);
